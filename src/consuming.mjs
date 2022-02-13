016400fb4ed2bc76c355605669d92ab95eb9ed29
Jarda Bílek <jarbilek@gmail.com>
2022-02-13 09:05:34 +0100

    performing one last operation

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,32 +1,64 @@
-import setText, {appendText, showWaiting, hideWaiting} from './results.mjs';
+import setText, { appendText, showWaiting, hideWaiting } from "./results.mjs";
 
 export function get() {
-    axios.get("http://localhost:3000/orders/1")
-        .then(({ data })=> {
-        setText(JSON.stringify(data));
-        })    ;
+  axios.get("http://localhost:3000/orders/1").then(({ data }) => {
+    setText(JSON.stringify(data));
+  });
 }
 
 export function getCatch() {
-    axios.get("http://localhost:3000/orders/123").then(({ data }) => {
-        setText(JSON.stringify(data));
+  axios
+    .get("http://localhost:3000/orders/123")
+    .then(({ data }) => {
+      setText(JSON.stringify(data));
     })
-        .catch(err => setText(err));
+    .catch((err) => setText(err));
 }
 
 export function chain() {
-    axios.get("http://localhost:3000/orders/1")
-        .then(({ data }) => {
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({ data }) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`
+      );
+    })
+    .then(({ data }) => {
+      setText(`City: ${data.city}`);
+    });
+}
 
-            return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+export function chainCatch() {
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({ data }) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`
+      );
+    })
+    .then(({ data }) => {
+      setText(`City: ${data.my.city}`);
+    })
+    .catch((err) => setText(err));
+}
+export function final() {
+    showWaiting();
+      axios
+        .get("http://localhost:3000/orders/1")
+        .then(({ data }) => {
+          return axios.get(
+            `http://localhost:3000/addresses/${data.shippingAddress}`
+          );
         })
         .then(({ data }) => {
-    
-                setText(`City: ${data.city}`);
-            });
-}
+          setText(`City: ${data.city}`);
+        })
+          .catch((err) => setText(err))
+          .finally(() => {
+              setTimeout(() => {
+                  hideWaiting();
+              }, 1500);
 
-export function chainCatch(){
-}
-export function final(){
+              appendText("  -- Completely done")
+          });
 }
